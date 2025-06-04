296625633e5813d854bbbf3917efb203fe92f674
Javier Ruiz Serrano <javier.ruiz.serrano59@gmail.com>
2025-06-04 10:00:57 +0200

    Fix en token

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-import './assets/main.css'
-
+// main.js
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import { createPinia } from 'pinia'
+import { useAuthStore } from '@/stores/auth'
 
+const app = createApp(App)
 
-const app = createApp(App);
+const pinia = createPinia()
+app.use(pinia)
+app.use(router)
 
-app.use(createPinia());
-app.use(router);
+app.mount('#app')
 
-app.mount('#app');
+// 👇 Inicia la autenticación tras montar la app
+const authStore = useAuthStore()
+;(async () => {
+    await authStore.initAuth()
+})()
