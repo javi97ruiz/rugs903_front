f1f1373af6a391f8db5f39a7be2c015fef1a05ba
javi97ruiz <javier.ruiz.serrano59@gmail.com>
2025-06-05 18:11:09 +0200

    Cambio en footer position page

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -33,7 +33,7 @@
 
 <style>
 .footer {
-  position: fixed;
+  position: page;
   width: 100vw;
   margin-left: calc(-50vw + 50% ); /* Corrige el desplazamiento lateral */
   background-color: #282828;
