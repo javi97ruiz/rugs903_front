6461b59f341080c663622e82019a375f18a6ffc2
javi97ruiz <javier.ruiz.serrano59@gmail.com>
2025-06-11 13:52:10 +0200

    cambio en imagen carrusel

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -19,7 +19,7 @@ import {
 } from 'vue-flux';
 
 // Importa las imágenes correctamente
-import brandImage from '@/assets/brand.jpeg';
+import brandImage from '@/assets/simpson.webp';
 import exampleImage from '@/assets/65f73fd5f2d5e1.63535676.jpg'
 import example2Image from '@/assets/shopping.webp'
 
